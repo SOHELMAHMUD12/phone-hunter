974461eec7dbf3fe5f5eb39d08483a6a1048daf3
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-30 20:06:59 +0600

    added loader spinner

--- js/app.js
+++ js/app.js
@@ -9,6 +9,21 @@ const displayPhones = phones =>{
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
+    // display 20pnone only
+    phones = phones.slice(0, 25);
+
+    // display no phones found
+    const noPhone = document.getElementById('no-found');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+
+    // display all phones
+
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -24,14 +39,32 @@ const displayPhones = phones =>{
         phonesContainer.appendChild(phoneDiv);
         
     })
-
-    document.getElementById('btn-search').addEventListener('click', function(){
-        const searchField = document.getElementById('search-field');
-        const searchText = searchField.value;
-        loadPhones(searchText);
-    })
+ // stop loader
+ toggleSpinner(false)
 
 }
+
+
+// handle seach button click
+ document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+})
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
 
 loadPhones("");
 
